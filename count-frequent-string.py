f3944c5646dede0c35b7a7e082663c908be524e9
Ricardo Martins <ricardomartins@Delorean.local>
2017-12-14 14:45:19 +0000

    add script to count frequent strings

---
+++ count-frequent-string.py
@@ -0,0 +1,18 @@
+# place your file in the directory of this script
+# replace test.txt with file to count frequent strings
+
+import re
+import string
+frequency = {}
+document_text = open('test.txt', 'r')
+text_string = document_text.read().lower()
+match_pattern = re.findall(r'\b[a-z]{3,15}\b', text_string)
+
+for word in match_pattern:
+    count = frequency.get(word,0)
+    frequency[word] = count + 1
+
+frequency_list = frequency.keys()
+
+for words in frequency_list:
+    print words, frequency[words]
